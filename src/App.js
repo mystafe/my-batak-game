375f852ee246b8927d778d3a97ee523dd27a2412
mystafe <admess34@gmail.com>
2024-08-19 00:50:10 +0400

    final 2.5.1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <GameBoard />
-      <h6 className="game-title">Batak: The Card Game - Under Development</h6>
+      <h6 className="game-title">Batak: The Card Game - Under Development - by Mustafa Evleksiz</h6>
     </div>
   );
 }
